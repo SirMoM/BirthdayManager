eba40de3661d77e164762c53c048ef774971216a
Noah Ruben <noah.ruben@online.de>
2018-12-20 13:12:37 +0100

    -initial commit

---
+++ BirthdayManager/src/main/java/application/controller/MainController.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package application.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Noah Ruben
+ *
+ */
+public class MainController{
+
+	private Stage stage;
+
+	public MainController(Stage stage){
+		this.stage = stage;
+	}
+
+	public Stage getStage(){
+		return this.stage;
+	}
+
+	public void gotoNextScene(String fxmlPath, Initializable controller){
+		try{
+			this.replaceSceneContent(fxmlPath, controller);
+		} catch (Exception ex){
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	public void setStage(Stage stage){
+		this.stage = stage;
+	}
+
+	public void start(){
+		try{
+			this.replaceSceneContent("/application/view/BirthdaysOverview.fxml", new BirthdaysOverviewController(this));
+
+		} catch (Exception ex){
+			System.out.println("Login: " + ex.getMessage());
+		}
+	}
+
+	private void replaceSceneContent(String fxmlPath, Initializable controller) throws Exception{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(this.getClass().getResource(fxmlPath));
+		loader.setController(controller);
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		this.stage.setScene(scene);
+
+		// Show the GUI
+		this.stage.show();
+	}
+
+}
---
+++ BirthdayManager/src/main/java/application/processes/openBirthdayFile.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package application.processes;
+
+import java.io.File;
+
+import javafx.concurrent.Task;
+
+/**
+ * @author Noah Ruben
+ * @see <a href="https://github.com/SirMoM/BirthdayManager">Github</a>
+ */
+public class openBirthdayFile extends Task<File>{
+
+	@Override
+	protected File call() throws Exception{
+		return null;
+	}
+
+}
