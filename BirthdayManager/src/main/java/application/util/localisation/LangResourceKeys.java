ccf99f4e707f615cd8d74c19e2f44e460a689b5d
Noah Ruben <noah.ruben@online.de>
2019-01-03 10:33:59 +0100

    added a lot XD

---
+++ BirthdayManager/src/main/java/application/util/localisation/LangResourceKeys.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package application.util.localisation;
+
+/**
+ * @author Noah Ruben
+ * @see <a href="https://github.com/SirMoM/BirthdayManager">Github</a>
+ */
+public enum LangResourceKeys{
+
+	str_nextBirthday_Label,
+	str_recentBirthday_Label,
+
+	file_menu,
+	openFile_MenuItem,
+	openRecent_MenuItem,
+	closeFile_MenuItem,
+	saveFile_MenuItem,
+	saveAsFile_MenuItem,
+	exporteFile_MenuItem,
+	preferences_MenuItem,
+	quit_MenuItem,
+
+	edit_menu,
+	showNextBirthdays_MenuItem,
+	showLastBirthdays_MenuItem,
+	newBirthday_MenuItem,
+	importBirthdays_MenuItem,
+	deleteBirthdays_MenuItem,
+
+	help_menu,
+
+	// List menu
+	openBirthday_MenuItem,
+
+	week_tap,
+	monday_column1,
+	tuesday_column1,
+	wednesday_column1,
+	thursday_column1,
+	friday_column1,
+	saturday_column1,
+	sunday_column1,
+
+	month_tap,
+	monday_column2,
+	tuesday_column2,
+	wednesday_column2,
+	thursday_column2,
+	friday_column2,
+	saturday_column2,
+	sunday_column2;
+
+}
