ed5975fddd2527e8e03e3ba60a43b1e362fe9005
Noah Ruben <noah.ruben@online.de>
2019-05-13 19:59:35 +0200

    - javadoc fixes

--- BirthdayManager/src/main/java/application/processes/UpdateBirthdaysThisWeekTask.java
+++ BirthdayManager/src/main/java/application/processes/UpdateBirthdaysThisWeekTask.java
@@ -21,7 +21,7 @@ import javafx.concurrent.Task;
  * @author Admin
  * @see <a href="https://github.com/SirMoM/BirthdayManager">Github</a>
  */
-public class UpdateBirthdaysThisWeekTask extends Task<List<PersonsInAWeek>> {
+public class UpdateBirthdaysThisWeekTask extends Task<List<PersonsInAWeek>>{
 
 	private final Logger LOG;
 	private List<Person> personDB = null;
@@ -30,44 +30,42 @@ public class UpdateBirthdaysThisWeekTask extends Task<List<PersonsInAWeek>> {
 	/**
 	 *
 	 */
-	public UpdateBirthdaysThisWeekTask() {
+	public UpdateBirthdaysThisWeekTask(){
 		this.LOG = LogManager.getLogger(this.getClass().getName());
-		week = LocalDate.now().get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
-		this.LOG.info("Gathering the Birthdays for this week: " + week);
+		this.week = LocalDate.now().get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
+		this.LOG.info("Gathering the Birthdays for this week: " + this.week);
 
-		if (PersonManager.getInstance().getPersons() != null && !PersonManager.getInstance().getPersons().isEmpty()) {
-			personDB = PersonManager.getInstance().getPersons();
+		if(PersonManager.getInstance().getPersons() != null && !PersonManager.getInstance().getPersons().isEmpty()){
+			this.personDB = PersonManager.getInstance().getPersons();
 		}
 	}
 
 	/**
 	 * Fills the BirthdaysThisWeek
 	 *
-	 * @param temp the {@link List} of persons where the Birthdays this week are
-	 *             extracted
 	 * @see javafx.concurrent.Task#call()
 	 */
 	@Override
-	protected List<PersonsInAWeek> call() throws Exception {
-		LOG.debug("Started " + this.getClass().getName());
-		while (personDB == null || PersonManager.getInstance().getPersons().isEmpty()) {
-			personDB = PersonManager.getInstance().getPersons();
-			LOG.warn("Waiting for personenDB to be filled!");
+	protected List<PersonsInAWeek> call() throws Exception{
+		this.LOG.debug("Started " + this.getClass().getName());
+		while (this.personDB == null || PersonManager.getInstance().getPersons().isEmpty()){
+			this.personDB = PersonManager.getInstance().getPersons();
+			this.LOG.warn("Waiting for personenDB to be filled!");
 			Thread.sleep(500);
 		}
 
 		final List<Person> birthdaysThisWeek = new ArrayList<Person>();
 
-		personDB.sort(new BirthdayComparator(true));
+		this.personDB.sort(new BirthdayComparator(true));
 
-		for (final Person person : personDB) {
+		for(final Person person : this.personDB){
 			final LocalDate birthday = person.getBirthday().withYear(2019);
-			if (birthday.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR) == week) {
+			if(birthday.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR) == this.week){
 				birthdaysThisWeek.add(person);
 			}
 		}
 
-		LOG.debug("Gathered all Persons this week( " + week + " )! Count: " + birthdaysThisWeek.size());
+		this.LOG.debug("Gathered all Persons this week( " + this.week + " )! Count: " + birthdaysThisWeek.size());
 		final List<PersonsInAWeek> personsInAWeekList = PersonsInAWeek.parseAList(birthdaysThisWeek);
 		return personsInAWeekList;
 	}
