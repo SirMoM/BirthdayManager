4c92f9ac29ffe1c14b51e3a11e676ad4ca77211e
Noah Ruben <pirat99@online.de>
2021-04-17 12:31:32 +0200

    --asd

--- BirthdayManager/src/main/java/application/controller/BirthdaysOverviewController.java
+++ BirthdayManager/src/main/java/application/controller/BirthdaysOverviewController.java
@@ -7,13 +7,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
@@ -21,7 +18,7 @@ import javafx.scene.text.Font;
  * @author Noah Ruben
  * @see <a href="https://github.com/SirMoM/BirthdayManager">Github</a>
  */
-public class BirthdaysOverviewController extends Controller{
+public class BirthdaysOverviewController extends Controller {
 	@FXML // ResourceBundle that was given to the FXMLLoader
 	private ResourceBundle resources;
 
@@ -40,27 +37,27 @@ public class BirthdaysOverviewController extends Controller{
 	@FXML // fx:id="x4"
 	private Color x4; // Value injected by FXMLLoader
 
-	public BirthdaysOverviewController(MainController mainController){
+	public BirthdaysOverviewController(MainController mainController) {
 		super(mainController);
 	}
 
-	final EventHandler<Event> openHandler = new EventHandler<Event>() {
+	final EventHandler<ActionEvent> openHandler = new EventHandler<ActionEvent>() {
 		@Override
-		public void handle(Event event) {
-			if (event.getEventType() != ActionEvent.ANY) {
-				if (!((KeyEvent) event).getCode().equals(KeyCode.ENTER)) {
-					return;
-				}
-			}
+		public void handle(ActionEvent event) {
+			System.out.println("pressed");
+
 		}
+
 	};
-	
-	@Override
+
 	@FXML // This method is called by the FXMLLoader when initialization is complete
-	public void initialize(URL location, ResourceBundle resources){
+	public void initialize(URL location, ResourceBundle resources) {
 		assert this.openFileMenuItem != null : "fx:id=\"openFileMenuItem\" was not injected: check your FXML file 'Kalender.fxml'.";
 		assert this.nextBirthdayLabel != null : "fx:id=\"nextBirthdayLabel\" was not injected: check your FXML file 'Kalender.fxml'.";
 		assert this.x3 != null : "fx:id=\"x3\" was not injected: check your FXML file 'Kalender.fxml'.";
 		assert this.x4 != null : "fx:id=\"x4\" was not injected: check your FXML file 'Kalender.fxml'.";
+
+		openFileMenuItem.addEventHandler(ActionEvent.ANY, this.openHandler);
+
 	}
 }
--- BirthdayManager/src/main/java/application/controller/Controller.java
+++ BirthdayManager/src/main/java/application/controller/Controller.java
@@ -9,15 +9,15 @@ import javafx.fxml.Initializable;
  * @author Noah Ruben
  * @see <a href="https://github.com/SirMoM/BirthdayManager">Github</a>
  */
-public abstract class Controller implements Initializable{
+public abstract class Controller implements Initializable {
 
 	private final MainController mainController;
 
-	public Controller(MainController mainController){
+	public Controller(MainController mainController) {
 		this.mainController = mainController;
 	}
 
-	public MainController getMainController(){
+	public MainController getMainController() {
 		return this.mainController;
 	}
 }
