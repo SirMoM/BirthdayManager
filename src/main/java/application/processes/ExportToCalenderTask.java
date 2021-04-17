543c38cb8d86971d3feed26eb1edd6adc0e59164
Noah Ruben <pirat99@online.de>
2021-04-17 13:40:25 +0200

    Restructure project

---
+++ src/main/java/application/processes/ExportToCalenderTask.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package application.processes;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import application.model.Person;
+import application.model.PersonManager;
+import application.util.PropertyFields;
+import application.util.PropertyManager;
+import biweekly.ICalVersion;
+import biweekly.ICalendar;
+import biweekly.component.VAlarm;
+import biweekly.component.VEvent;
+import biweekly.io.text.ICalWriter;
+import biweekly.parameter.Related;
+import biweekly.property.Action;
+import biweekly.property.Classification;
+import biweekly.property.Summary;
+import biweekly.property.Trigger;
+import biweekly.util.Duration;
+import javafx.concurrent.Task;
+
+/**
+ * @author Noah Ruben
+ *
+ */
+public class ExportToCalenderTask extends Task<Boolean> {
+
+	ICalendar ical;
+	ICalWriter writer;
+	Boolean succsessfull = true;
+
+	public ExportToCalenderTask(File file) throws IOException {
+		this.ical = new ICalendar();
+		writer = new ICalWriter(file, ICalVersion.V2_0);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javafx.concurrent.Task#call()
+	 */
+	@Override
+	protected Boolean call() throws Exception {
+		createCalender();
+		if (writer != null && ical != null) {
+			writer.write(ical);
+		} else {
+			succsessfull = false;
+		}
+		close();
+		return succsessfull;
+	}
+
+	private void close() {
+		try {
+			writer.close();
+		} catch (IOException ioException) {
+			succsessfull = false;
+		}
+	}
+
+	private void createCalender() {
+		int size = PersonManager.getInstance().getPersons().size();
+		for (int i = 0; i < size; i++) {
+			Person person = PersonManager.getInstance().getPersons().get(i);
+			VEvent event = new VEvent();
+			Summary summary = event.setSummary(person.namesToString());
+			summary.setLanguage("de-DE");
+			Date start = Date
+					.from(person.getBirthday().withYear(LocalDate.now().getYear()).atStartOfDay(ZoneId.systemDefault()).toInstant());
+			event.setDateStart(start, false);
+			event.setClassification(Classification.PUBLIC);
+
+			if (new Boolean(PropertyManager.getProperty(PropertyFields.EXPORT_WITH_ALARM))) {
+				Duration dayBefore = Duration.builder().prior(true).days(1).build();
+				Duration onBirthday = Duration.builder().prior(false).hours(8).build();
+				event.addAlarm(new VAlarm(Action.display(), new Trigger(dayBefore, Related.START)));
+				event.addAlarm(new VAlarm(Action.display(), new Trigger(onBirthday, Related.START)));
+			}
+
+			this.ical.addEvent(event);
+			updateProgress(i, size);
+		}
+
+	}
+
+}
---
+++ src/main/java/application/processes/UpdateNextBirthdaysTask.java
@@ -0,0 +1,121 @@
+/**
+ *
+ */
+package application.processes;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import application.model.Person;
+import application.model.PersonManager;
+import application.util.BirthdayComparator;
+import application.util.PropertyFields;
+import application.util.PropertyManager;
+import javafx.concurrent.Task;
+
+/**
+ * @author Admin
+ * @see <a href="https://github.com/SirMoM/BirthdayManager">Github</a>
+ */
+public class UpdateNextBirthdaysTask extends Task<List<Person>> {
+
+	/** Logger for this process */
+	private final Logger LOG;
+
+	/** List of all persons / birthdays */
+	private List<Person> personDB;
+
+	/** How many birthdays will be shown. */
+	private final int NEXT_BIRTHDAYS_COUNT;
+
+	/** How long the process has bee waiting */
+	private int timeInWaiting;
+
+	/** Default Constructor */
+	public UpdateNextBirthdaysTask() {
+		this.LOG = LogManager.getLogger(this.getClass().getName());
+
+		NEXT_BIRTHDAYS_COUNT = Integer.parseInt(PropertyManager.getProperty(PropertyFields.SHOW_BIRTHDAYS_COUNT));
+
+		if (PersonManager.getInstance().getPersons() != null && !PersonManager.getInstance().getPersons().isEmpty()) {
+			personDB = PersonManager.getInstance().getPersons();
+		}
+		this.updateProgress(0, NEXT_BIRTHDAYS_COUNT);
+	}
+
+	/**
+	 * Fills the NextBirthdays List
+	 *
+	 * @return A {@link List} of persons
+	 *
+	 * @see javafx.concurrent.Task#call()
+	 */
+	@Override
+	protected List<Person> call() throws Exception {
+		LOG.debug("Started " + this.getClass().getName() + " " + System.currentTimeMillis());
+
+		// Checks if Data is there else aborts this Task.
+		while (personDB == null || PersonManager.getInstance().getPersons().isEmpty()) {
+			personDB = PersonManager.getInstance().getPersons();
+			LOG.info("Waiting for personenDB to be filled!");
+
+			Thread.sleep(500);
+			this.timeInWaiting += 500;
+			if (this.timeInWaiting > 10000) {
+				this.cancel();
+				LOG.debug("Thread canceled because it took too long to wait for the list of people!");
+			}
+		}
+
+		/** All upcomming persons / birthdays */  
+		final List<Person> upcomming = new ArrayList<Person>();
+
+		/** All passed persons / birthdays */  
+		final List<Person> after = new ArrayList<Person>();
+		/** Final next persons / birthdays */
+		final List<Person> nextBirthdays = new ArrayList<Person>();
+
+		// Fill upcomming and after based on today
+		for (int i = 0; i < personDB.size(); i++) {
+			final Person person = personDB.get(i);
+			int dayOfYear = person.getBirthday().withYear(LocalDate.now().getYear()).getDayOfYear();
+			if (dayOfYear >= LocalDate.now().getDayOfYear()) {
+				upcomming.add(person);
+			} else {
+				after.add(person);
+			}
+		}
+
+		upcomming.sort(new BirthdayComparator(false));
+		after.sort(new BirthdayComparator(false));
+
+		int i = 0;
+		for (; i < NEXT_BIRTHDAYS_COUNT; i++) {
+			try {
+				nextBirthdays.add(upcomming.get(i));
+				this.updateProgress(i, NEXT_BIRTHDAYS_COUNT);
+			} catch (final IndexOutOfBoundsException indexOutOfBoundsException) {
+				this.LOG.debug("Probably not enought Persons to gather the 10 birthdays for next",
+						indexOutOfBoundsException);
+				break;
+			}
+		}
+
+		for (int j = 0; j < NEXT_BIRTHDAYS_COUNT - i; j++) {
+			try {
+				nextBirthdays.add(after.get(j));
+				this.updateProgress(j, NEXT_BIRTHDAYS_COUNT);
+			} catch (final IndexOutOfBoundsException indexOutOfBoundsException) {
+				this.LOG.debug("Probably not enought Persons to gather the 10 birthdays for next",
+						indexOutOfBoundsException);
+				break;
+			}
+		}
+		return nextBirthdays;
+	}
+
+}
