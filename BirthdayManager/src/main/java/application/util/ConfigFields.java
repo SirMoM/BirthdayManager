1f015f3eb7d98ad552c94feb9c0af68eafa35ba9
Noah Ruben <pirat99@online.de>
2021-04-17 12:31:36 +0200

    add opening File works sorta

---
+++ BirthdayManager/src/main/java/application/util/ConfigFields.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package application.util;
+
+/**
+ * @author Noah Ruben
+ * @see <a href="https://github.com/SirMoM/BirthdayManager">Github</a>
+ */
+public interface ConfigFields{
+
+	final String LAST_OPEND = "last_opend";
+
+}
